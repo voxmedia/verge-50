b21cf9fdda07a276da71dd599d9bf6baf883dbb6
Guillermo Esteves <guillermo.esteves@sbnation.com>
2013-12-03 15:05:50 -0500

    Add a basic header and side nav

---
+++ source/javascripts/verge/nav.js
@@ -0,0 +1,40 @@
+var Verge = Verge || {};
+
+Verge.Nav = (function ($) {
+
+  var $nav = $('.m-nav'),
+      $toggle = $('.m-header__nav-toggle'),
+      $body = $('body'),
+      open_class = 'open-nav';
+
+  var openNav = function () {
+    $body.addClass(open_class);
+    return false;
+  };
+
+  var closeNav = function () {
+    $body.removeClass(open_class);
+    return false;
+  };
+
+  var toggleNav = function () {
+    if ($body.hasClass(open_class)) {
+      closeNav();
+    } else {
+      openNav();
+    }
+    return false;
+  };
+
+  var init = function () {
+    $toggle.on('click', toggleNav);
+  };
+
+  init();
+
+  return {
+    openNav : openNav,
+    closeNav : closeNav,
+    toggleNav : toggleNav
+  }
+})(jQuery);
