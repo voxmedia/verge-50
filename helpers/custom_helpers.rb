af26a3aeaf79e894d52575f33ca656b7cdd779ca
Guillermo Esteves <guillermo.esteves@sbnation.com>
2013-12-04 17:48:25 -0500

    Change the URL helpers one last time

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -7,4 +7,20 @@ module CustomHelpers
       "The Verge Fifty &ndash; #{person['name']}"
     end
   end
+
+  def feature_url(person = nil)
+    if person.nil?
+      return "#{url_prefix}"
+    else
+      return "#{url_prefix}/#{person['slug']}"
+    end
+  end
+
+  def absolute_feature_url(person = nil)
+    if person.nil?
+      return "#{absolute_prefix}#{url_prefix}"
+    else
+      return "#{absolute_prefix}#{url_prefix}/#{person['slug']}"
+    end
+  end
 end
