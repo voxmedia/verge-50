f5d8add17b3aeb19de4dbeb9f924142a4581dd5f
Guillermo Esteves <guillermo.esteves@sbnation.com>
2013-12-04 17:33:24 -0500

    One more change to the helpers

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -7,12 +7,4 @@ module CustomHelpers
       "The Verge Fifty &ndash; #{person['name']}"
     end
   end
-
-  def page_url(person)
-    if person.nil?
-      "http://www.theverge.com#{url_for("/")}"
-    else
-      "http://www.theverge.com/#{url_for(person['slug'], :relative => true)}"
-    end
-  end
 end
