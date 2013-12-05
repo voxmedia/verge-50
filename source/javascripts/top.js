aeaf71dd155d4d86eaec0f4afea5e5c8aafa9b5a
Brian Anderson <banderson623@gmail.com>
2013-12-05 00:41:23 -0600

    trivial attempt falling apart

--- source/javascripts/top.js
+++ source/javascripts/top.js
@@ -4,13 +4,20 @@ var Verge = Verge || {};
 
 Vox.EditorialApps.Ads.setup();
 
-// Will be called when Hymnal is going to give you code
-function insertAdCode(adCode){
-  console.log('hymnal gave me code!');
-  var $before_ad = jQuery('li.ad_next');
+// Will be called when Hymnal is going to give you html to insert
+function insertAdCode(ev,adCode){
+  console.log('hymnal gave me code!',adCode);
+  var html = "<li class='m_pages_page ad'>" + adCode + "</li>";
+  jQuery().ready(function(){
+    console.log('inserting into',adCode);
+    jQuery(html).insertAfter('li.ad_next');
+    console.log(jQuery('li.ad')[0]);
+  });
+  // should probably NOT call page builder before this
 }
 
-jQuery.ready(function(){
-  console.log('ready');
-  jQuery(document).on('Hymnal.insertAdCode',insertAdCode);
-});
+jQuery(document).on('Hymnal.insertAdCode',insertAdCode);
+
+
+// Test with
+jQuery(document).trigger('Hymnal.insertAdCode',"<h1>Hi</h1>");
