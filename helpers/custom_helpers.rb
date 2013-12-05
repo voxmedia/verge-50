8341ebc27c584e52665ccde18c204e1ee795d77c
Guillermo Esteves <guillermo.esteves@sbnation.com>
2013-12-05 13:35:47 -0500

    Add a horrible method to generate rel="prev" and rel="next" meta tags

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -45,6 +45,32 @@ module CustomHelpers
     end
   end
 
+  # Oh god this is horrible
+  def rel_prev_next_links(page = nil, person = nil)
+    if !person.nil?
+      page = 'list'
+    end
+
+    case page
+    when 'index'
+      "<link rel='next' href='#{absolute_page_url('intro')}' />"
+    when 'intro'
+      "<link rel='prev' href='#{absolute_page_url('index')}' />\n" \
+      "<link rel='next' href='#{absolute_page_url('list', data.content.people.first)}' />"
+    when 'credits'
+      "<link rel='prev' href='#{absolute_page_url('list', data.content.people.last)}' />"
+    when 'list'
+      index = data.content.people.index(person)
+      if index == 0
+        "<link rel='prev' href='#{absolute_page_url('intro')}' />\n" \
+        "<link rel='next' href='#{absolute_page_url('list', data.content.people[index + 1])}' />"
+      elsif index == data.content.people.size - 1
+        "<link rel='prev' href='#{absolute_page_url('list', data.content.people[index - 1])}' />\n" \
+        "<link rel='next' href='#{absolute_page_url('credits')}' />"
+      else
+        "<link rel='prev' href='#{absolute_page_url('list', data.content.people[index - 1])}' />\n" \
+        "<link rel='next' href='#{absolute_page_url('list', data.content.people[index + 1])}' />"
+      end
     end
   end
 end
