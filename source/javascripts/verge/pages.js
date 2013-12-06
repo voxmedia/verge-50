5528f1e15db331309807262f94ba19a3acbdcb48
Guillermo Esteves <guillermo.esteves@sbnation.com>
2013-12-06 16:41:04 -0500

    I cannot believe how much JS I had to write to deal with Safari's bullshit

--- source/javascripts/verge/pages.js
+++ source/javascripts/verge/pages.js
@@ -69,7 +69,7 @@ Verge.Pages = (function ($) {
 
     current = index;
 
-    $next_page = $pages.eq(current).addClass('current');
+    $next_page = $pages.eq(current).removeAttr('style').addClass('current');
 
     $current_page.addClass(out_class).on(animation_end_event, function() {
       $current_page.off(animation_end_event);
@@ -155,6 +155,7 @@ Verge.Pages = (function ($) {
     end_next_page = false;
     is_animating = false;
     $in_page.attr({ class : $in_page.data().original_class + ' current' });
+    $in_page.css('overflow-y', 'auto');
     if (typeof $out_page !== 'undefined') {
       $out_page.attr({ class : $out_page.data().original_class }).scrollTop(0);
     }
