6ecb6dbe6a664dd0dce2f2f8055c3c83bb8d6eda
Brian Anderson <banderson623@gmail.com>
2013-12-05 23:52:37 -0600

    more crazy

--- source/javascripts/top.js
+++ source/javascripts/top.js
@@ -3,21 +3,3 @@ var Verge = Verge || {};
 
 
 Vox.EditorialApps.Ads.setup();
-
-// Will be called when Hymnal is going to give you html to insert
-function insertAdCode(ev,adCode){
-  console.log('hymnal gave me code!',adCode);
-  var html = "<li class='m_pages_page ad'>" + adCode + "</li>";
-  jQuery().ready(function(){
-    console.log('inserting into',adCode);
-    jQuery(html).insertAfter('li.ad_next');
-    console.log(jQuery('li.ad')[0]);
-  });
-  // should probably NOT call page builder before this
-}
-
-jQuery(document).on('Hymnal.insertAdCode',insertAdCode);
-
-
-// Test with
-jQuery(document).trigger('Hymnal.insertAdCode',"<h1>Hi</h1>");
