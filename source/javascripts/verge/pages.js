68513e1137d3d3918f31bfdc314d9c16ea648bf9
Guillermo Esteves <guillermo.esteves@sbnation.com>
2013-12-08 21:12:06 -0500

    Allow middle/cmd/ctrl-clicks in main logo

--- source/javascripts/verge/pages.js
+++ source/javascripts/verge/pages.js
@@ -212,9 +212,11 @@ Verge.Pages = (function ($) {
     $previous.on('click', previousPage);
     $page_links.on('click', clickToPage);
     $(document).on('keydown', keyboardNav);
-    $('.m-header__logo').click('a', function () {
-      goToPage(0);
-      return false;
+    $('.m-header__logo').click('a', function (e) {
+      if (e.which === 1 && !e.metaKey && !e.ctrlKey) {
+        e.preventDefault();
+        goToPage(0);
+      }
     })
 
     $pages.each(function() {
